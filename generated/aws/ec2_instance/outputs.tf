e3b8620b49adc2f8c94c006969c82af36da9cf60
toberosa <55308481+toberosa@users.noreply.github.com>
2023-09-12 03:36:52 +0800

    Delete generated/aws/ec2_instance directory

--- generated/aws/ec2_instance/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_instance_tfer--i-02f15bc709354c668_webterraformer_id" {
-  value = "${aws_instance.tfer--i-02f15bc709354c668_webterraformer.id}"
-}
--- generated/aws/ec2_instance/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {}
-
-terraform {
-	required_providers {
-		aws = {
-	    version = "~> 5.15.0"
-		}
-  }
-}
